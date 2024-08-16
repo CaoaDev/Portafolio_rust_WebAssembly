609199c1f6d7b9947712a6f778cf5260318718c3
CaoaDev <caoa_sinos9@outlook.es>
2024-08-16 17:08:58 -0600

    add fn to skills

--- src/components/experience/experience_module.rs
+++ src/components/experience/experience_module.rs
@@ -33,61 +33,14 @@ pub fn Experience() -> impl IntoView {
 fn frontend_skills() -> impl IntoView {
     view! {
         <>
-            <article class="experience__details">
-                <a href="https://www.w3.org/" target="_blank" class="experience__details-link">
-                <Icon icon=AiHtml5Outlined class="experience__details-icon" />
-                    <h4>"HTML +3"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.w3.org/Style/CSS/Overview.en.html" target="_blank" class="experience__details-link">
-                <Icon icon=BsFiletypeCss class="experience__details-icon" />
-                    <h4>"CSS/SCSS/SASS +2"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://ecma-international.org/publications-and-standards/standards/ecma-262/" target="_blank" class="experience__details-link">
-                <Icon icon=TbBrandJavascript class="experience__details-icon" />
-                    <h4>"JavaScript +2"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://reactjs.org/" target="_blank" class="experience__details-link">
-                <Icon icon=BiReact class="experience__details-icon" />
-            <h4>"React/Next +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://redux.js.org/" target="_blank" class="experience__details-link">
-                    <Icon icon=SiRedux class="experience__details-icon" />
-                    <h4>"Redux +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.java.com/" target="_blank" class="experience__details-link">
-                    <Icon icon=FaJavaBrands class="experience__details-icon" />
-                    <h4>"JAVA +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.php.net/" target="_blank" class="experience__details-link">
-                    <Icon icon=TbFileTypePhp class="experience__details-icon" />
-                    <h4>"PHP +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://svelte.dev/" target="_blank" class="experience__details-link">
-                    <Icon icon=SiSvelte class="experience__details-icon" />
-                    <h4>"Svelte +1"</h4>
-                </a>
-            </article>
+            {skill_item("HTML +3", "https://www.w3.org/", AiHtml5Outlined)}
+            {skill_item("CSS/SCSS/SASS +2", "https://www.w3.org/Style/CSS/Overview.en.html", BsFiletypeCss)}
+            {skill_item("JavaScript +2", "https://ecma-international.org/publications-and-standards/standards/ecma-262/", TbBrandJavascript)}
+            {skill_item("React/Next +1", "https://reactjs.org/", BiReact)}
+            {skill_item("Redux +1", "https://redux.js.org/", SiRedux)}
+            {skill_item("JAVA +1", "https://www.java.com/", FaJavaBrands)}
+            {skill_item("PHP +1", "https://www.php.net/", TbFileTypePhp)}
+            {skill_item("Svelte +1", "https://svelte.dev/", SiSvelte)}
         </>
     }
 }
@@ -95,81 +48,25 @@ fn frontend_skills() -> impl IntoView {
 fn backend_skills() -> impl IntoView {
     view! {
         <>
-            <article class="experience__details">
-                <a href="https://www.rust-lang.org/" target="_blank" class="experience__details-link">
-                    <Icon icon=FaRustBrands class="experience__details-icon" />
-                    <h4>"Rust/WebAssembly +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://nodejs.org/" target="_blank" class="experience__details-link">
-                    <Icon icon=BiNodejs class="experience__details-icon" />
-                    <h4>"Node/NestJs +3"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.postgresql.org/" target="_blank" class="experience__details-link">
-                    <Icon icon=SiPostgresql class="experience__details-icon" />
-                    <h4>"PostgreSQL +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://swagger.io/" target="_blank" class="experience__details-link">
-                    <Icon icon=SiSwagger class="experience__details-icon" />
-                    <h4>"Swagger/Jira +2"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.sql.org/" target="_blank" class="experience__details-link">
-                    <Icon icon=BsFiletypeSql class="experience__details-icon" />
-                    <h4>"SQL/NoSql +3"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://github.com/" target="_blank" class="experience__details-link">
-                    <Icon icon=VsGithub class="experience__details-icon" />
-                    <h4>"Git/GitHub +3"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://www.docker.com/" target="_blank" class="experience__details-link">
-                    <Icon icon=TbBrandDocker class="experience__details-icon" />
-                    <h4>"Docker +1"</h4>
-                </a>
-            </article>
-
-            <article class="experience__details">
-                <a href="https://aws.amazon.com/" target="_blank" class="experience__details-link">
-                    <Icon icon=FaAwsBrands class="experience__details-icon" />
-                    <h4>"AWS +1"</h4>
-                </a>
-            </article>
+            {skill_item("Rust/WebAssembly +1", "https://www.rust-lang.org/", FaRustBrands)}
+            {skill_item("Node/NestJs +3", "https://nodejs.org/", BiNodejs)}
+            {skill_item("PostgreSQL +1", "https://www.postgresql.org/", SiPostgresql)}
+            {skill_item("Swagger/Jira +2", "https://swagger.io/", SiSwagger)}
+            {skill_item("SQL/NoSql +3", "https://www.sql.org/", BsFiletypeSql)}
+            {skill_item("Git/GitHub +3", "https://github.com/", VsGithub)}
+            {skill_item("Docker +1", "https://www.docker.com/", TbBrandDocker)}
+            {skill_item("AWS +1", "https://aws.amazon.com/", FaAwsBrands)}
         </>
     }
 }
 
-// fn skill_item(skill: &'static str) -> impl IntoView {
-//     view! {
-//         <article class="experience__details">
-//             <BsFillPatchCheckFill class="experience__details-icon"/>
-//             <h4>{skill}</h4>
-//         </article>
-//     }
-// }
-
-// You'll need to implement this icon component or use an appropriate Leptos-compatible icon library
-#[component]
-fn BsFillPatchCheckFill(#[prop(into, optional)] class: Option<String>) -> impl IntoView {
-    let class = class.unwrap_or_default();
+fn skill_item(skill: &'static str, url: &'static str, icon: icondata::Icon) -> impl IntoView {
     view! {
-        <svg class={class} viewBox="0 0 16 16" fill="currentColor" height="1em" width="1em">
-            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
-        </svg>
+        <article class="experience__details">
+            <a href={url} target="_blank" class="experience__details-link">
+                <Icon icon={icon} class="experience__details-icon" />
+                <h4>{skill}</h4>
+            </a>
+        </article>
     }
 }
